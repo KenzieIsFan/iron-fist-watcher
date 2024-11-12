c5f9684fb679367a6ffdf193e3a65aa0e2ab97a5
KenzieIsFan <kenzie.fauzi@gmail.com>
2024-11-12 12:58:57 +0000

    Added search bar to header

--- iron-fist-watcher/src/components/Header.vue
+++ iron-fist-watcher/src/components/Header.vue
@@ -1,19 +1,17 @@
 <template> 
     <select v-model="observed_tour"  @change="set_select()">
-            
-            <option value="European TEKKEN Cup 3">European TEKKEN Cup 3</option>
-        
+                    
             <option value="TEKKEN World Tour 2024" >TEKKEN World Tour 2024</option>
         
     </select>
     
-
+    <SearchBar></SearchBar>
 </template>
 
 <script setup>
     import { useTemplateRef, ref,reactive, onMounted } from 'vue'
+import SearchBar from './SearchBar.vue';
     const observed_tour = defineModel("observed_tour")
-    const observed_players = defineModel("observed_players")
     const form = reactive({
     cat: ""
     })
--- iron-fist-watcher/src/components/SearchBar.vue
+++ iron-fist-watcher/src/components/SearchBar.vue
@@ -1,9 +1,26 @@
 <template>
-<input v-model="player_search" placeholder="Search for players here"/>
-</template>
+    <input type="text" v-model="input" placeholder="Search players in TWT..." />
+    <div v-if="input !== ''" >
+      <div  v-for="player in searchedPlayers" :key="player">
+        <p>{{ player }}</p>
+      </div>
+    </div>
+ </template>
 
-<script>
-import {ref} from 'vue';
-const player_search = ref('');
-
+<script setup>
+import { computed, onMounted, reactive, ref } from "vue";
+//Later will have to create a way for the 
+//players should be an object of name and db id for when it is selected 
+const players = reactive([{name:"apple"}, {name:"banana"}, {name:"orange"}]);
+let input = ref("");
+console.log(players)
+const searchedPlayers = computed(() => {
+      return players.filter((player) => {
+        return (
+          player.name
+            .toLowerCase()
+            .indexOf(input.value.toLowerCase()) != -1
+        );
+      });
+});
 </script>
